7c63fbd73b2c3e1a4c0f89e67a3b30a9f33c32b8
Andreas Woess <andreas.woess@oracle.com>
2022-11-22 20:21:52 +0100

    graaljs: pick the latest available release version

--- src/engines/graaljs.js
+++ src/engines/graaljs.js
@@ -27,6 +27,42 @@ function getArchiveExtension() {
   return platform.startsWith('win') ? '.zip' : '.tar.gz';
 }
 
+class GraalJSVersion {
+  constructor(tagName) {
+    const match = /((\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.(\d+))?.*)$/.exec(tagName);
+    if (match == null) {
+      throw new Error(`Unable to parse version from tag name '${tagName}'`);
+    }
+    const [, fullVersion, ...parts] = match;
+    this.fullVersion = fullVersion;
+    this.numParts = parts.filter((s) => s !== undefined).map((s) => parseInt(s, 10));
+  }
+
+  static from(a) {
+    return a instanceof GraalJSVersion ? a : new GraalJSVersion(a);
+  }
+
+  static compare(a, b) {
+    a = GraalJSVersion.from(a);
+    b = GraalJSVersion.from(b);
+    for (let i = 0; i < Math.max(a.numParts.length, b.numParts.length); i += 1) {
+      const cmp = Math.sign((a.numParts[i] || 0) - (b.numParts[i] || 0));
+      if (cmp !== 0) {
+        return cmp;
+      }
+    }
+    return a.fullVersion.localeCompare(b.fullVersion);
+  }
+
+  toString() {
+    return this.fullVersion;
+  }
+
+  get majorVersion() {
+    return this.numParts[0];
+  }
+}
+
 class GraalJSInstaller extends Installer {
   constructor(...args) {
     super(...args);
@@ -38,10 +74,14 @@ class GraalJSInstaller extends Installer {
     if (version === 'latest') {
       const body = await fetch('https://api.github.com/repos/oracle/graaljs/releases')
         .then((r) => r.json());
-      return body
+      const versions = body
         .filter((b) => !b.prerelease)
-        .sort((a, b) => new Date(b.published_at).getTime() - new Date(a.published_at).getTime())[0]
-        .tag_name.slice(3);
+        .map((b) => GraalJSVersion.from(b.tag_name))
+        .sort((a, b) => GraalJSVersion.compare(b, a));
+      if (versions.length === 0) {
+        throw new Error('Could not find a release version');
+      }
+      return versions[0].toString();
     }
     return version;
   }
